1cc18f357b45aab120512fa214bedc038335d6fb
liliu <liliu@signcc.com>
2020-04-05 18:53:41 +0800

    之前那个版本有些配置出了一点问题，这个版本是处理好了之后的，可供学习和参考

---
+++ src/test/java/com/imooc/miaosha/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.miaosha.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
